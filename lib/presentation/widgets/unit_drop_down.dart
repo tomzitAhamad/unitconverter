92d8c0cb017ad1019f1eedfdb4cd766170c26789
tomzit <barisakib6@gmail.com>
2026-04-07 23:30:27 +0600

    initial unit drop down

---
+++ lib/presentation/widgets/unit_drop_down.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class UnitDropdown extends StatelessWidget {
+  const UnitDropdown({
+    super.key,
+    required this.label,
+    required this.selectedUnit,
+    required this.units,
+    required this.onChanged,
+  });
+
+  final String label;
+  final String selectedUnit;
+  final List<String> units;
+  final ValueChanged<String?> onChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButtonFormField<String>(
+      initialValue: selectedUnit,
+      isExpanded: true,
+      decoration: InputDecoration(
+        labelText: label,
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
+        contentPadding: const EdgeInsets.symmetric(
+          horizontal: 12,
+          vertical: 14,
+        ),
+      ),
+      items: units
+          .map(
+            (unit) => DropdownMenuItem<String>(
+          value: unit,
+          child: Text(unit, overflow: TextOverflow.ellipsis),
+        ),
+      )
+          .toList(),
+      onChanged: onChanged,
+    );
+  }
+}
